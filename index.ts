f277ff5b2c04064b49decf45cb020d082f8457ee
eungook <yi.eungook@gmail.com>
2025-08-27 11:29:29 +0900

    feat: 서로 다른 URL에서 출간/업데이트 정보 정상적으로 가져오는지 확인하기
    - 만화책이 시리즈인지 단권인지에 따라 노출되는 정보가 다르다.

--- index.ts
+++ index.ts
@@ -4,37 +4,101 @@ import { JSDOM } from "jsdom";
  * - [x] fetch 해보기
  * - [x] 책 출간/업데이트 정보 가져오기
  * - [x] 해당 정보 Date로 파싱하기
+ * - [x] 서로 다른 URL에서 출간/업데이트 정보 정상적으로 가져오는지 확인하기
  */
 
+// const url = "https://ridibooks.com/books/505098346?_rdt_sid=new_release&_rdt_idx=0&_rdt_arg=comic";
+const urls = [
+    'https://ridibooks.com/books/505098346?_rdt_sid=new_release&_rdt_idx=0&_rdt_arg=comic',
+    'https://ridibooks.com/books/505098234?_rdt_sid=new_release&_rdt_idx=1&_rdt_arg=comic',
+    'https://ridibooks.com/books/297083309?_rdt_sid=new_release&_rdt_idx=2&_rdt_arg=comic',
+    'https://ridibooks.com/books/1690004072?_rdt_sid=new_release&_rdt_idx=3&_rdt_arg=comic',
+    'https://ridibooks.com/books/845051579?_rdt_sid=new_release&_rdt_idx=4&_rdt_arg=comic',
+    'https://ridibooks.com/books/4239000252?_rdt_sid=new_release&_rdt_idx=5&_rdt_arg=comic',
+    'https://ridibooks.com/books/505098248?_rdt_sid=new_release&_rdt_idx=6&_rdt_arg=comic',
+    'https://ridibooks.com/books/678027443?_rdt_sid=new_release&_rdt_idx=7&_rdt_arg=comic',
+    'https://ridibooks.com/books/806017187?_rdt_sid=new_release&_rdt_idx=8&_rdt_arg=comic',
+    'https://ridibooks.com/books/806017185?_rdt_sid=new_release&_rdt_idx=9&_rdt_arg=comic',
+    'https://ridibooks.com/books/806017188?_rdt_sid=new_release&_rdt_idx=10&_rdt_arg=comic',
+]
 
-const url = "https://ridibooks.com/books/505098346?_rdt_sid=new_release&_rdt_idx=0&_rdt_arg=comic";
-const body = await fetchBody(url);
-// console.log({ body: body.slice(0, 10000) });
+for (const url of urls) {
+    try {
+        await getBookInfo(url);
 
-const document = readBody(body);
-// console.log({
-//     "document.title": document.title,
-//     "document.body": document.body.innerHTML,
-//     // "리디 접속이 원활하지 않습니다." 라는 메세지가 p#access_check에 출력되지만, 이하 내용은 정상적으로 출력되는 것 같다.
-// });
-
-const islandsMetadata = document.querySelector("#ISLANDS__Metadata");
-if (!islandsMetadata) {
-    throw new Error("islandsMetadata not found");
+    } catch (error) {
+        console.error({
+            url,
+            error,
+        });
+    }
 }
 
-const time = islandsMetadata.querySelector("time");
-if (!time) {
-    throw new Error("time not found");
+/**
+ * 책 정보를 가져온다.
+ * - 책이 시리즈인지 단권인지 판단하여 출간/업데이트 정보를 가져온다.
+ * @param url 책 URL
+ */
+async function getBookInfo(url: string) {
+    // fetch
+    const body = await fetchBody(url);
+    const document = readBody(body);
+
+    // title
+    const documentTitle = document.title;
+    const title = documentTitle.split("-")[0]?.trim();
+
+    // date
+    let date: Date | null = null;
+    const islandsMetadata = document.querySelector("#ISLANDS__Metadata");
+    if (!islandsMetadata) {
+        throw new Error("islandsMetadata not found");
+    }
+    
+    // <time>: 업데이트 정보
+    const time = islandsMetadata.querySelector("time");
+    if (time) {
+        // 시리즈 후속권: 업데이트 정보가 있음
+        const datetime = time.getAttribute("datetime");
+        if (!datetime) {
+            throw new Error("datetime not found");
+        }
+        
+        date = new Date(datetime); // 형식: yyyy-mm-dd
+        date.setHours(0); // 참고: yyyy-mm-dd 형식이면 UTC 기준 0시로 처리된다. 즉 GMT+9라면 오전 9시로 처리된다. 그래서 setHours(0)으로 후처리한다.
+        
+        console.log({
+            title,
+            date,
+        });
+
+    } else {
+        // 시리즈 첫 권, 혹은 단권: 업데이트 정보가 없음
+        // - 대신 출간 정보가 있음
+        const li = islandsMetadata.querySelector('li'); // DOM 트리의 첫번째 li: 출간 정보
+        if (!li) {
+            throw new Error("li not found");
+        }
+
+        const textContent = li.textContent;
+        if (!textContent) {
+            throw new Error("textContent not found");
+        }
+
+        const textContent2 = textContent.split(" ")[0];
+        if (!textContent2) {
+            throw new Error("textContent2 not found");
+        }
+
+        date = new Date(textContent2); // 형식: yyyy.mm.dd // 참고: yyyy.mm.dd 형식이면 GMT 기준 0시로 처리된다.
+        
+        console.log({
+            title,
+            date,
+        });
+    }
 }
 
-const datetime = time.getAttribute("datetime");
-if (!datetime) {
-    throw new Error("datetime not found");
-}
-
-const date = new Date(datetime);
-console.log({ date });
 
 
 
